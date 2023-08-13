2a6803e234dcd871016a4c9d9531586f1bee9673
Kaito Kishi <17728957+TRSasasusu@users.noreply.github.com>
2023-08-14 01:25:59 +0900

    all sorts becomes generics.

--- src/sort.rs
+++ src/sort.rs
@@ -2,9 +2,6 @@ use std::collections::VecDeque;
 use pyo3::exceptions::PyTypeError;
 use pyo3::prelude::*;
 
-//fn _call_general_sort<F: Fn<T: Copy + PartialOrd>>(f: F, vec: PyObject) -> PyResult<PyObject> {
-//fn _call_general_sort<F>(f: F, vec: PyObject) where F: Fn(fn()) -> PyResult<PyObject> {
-//_call_each_type_sort<Fi: Fn(Vec<i64>), Ff: Fn(Vec<f64>), Fu8: Fn(<Vec<u8>>)>(fi: Fi, ff: Ff, fu8: Fu8) -> PyResult<PyObject> {
 fn _call_each_type_sort(fi: fn(&mut [i64]), ff: fn(&mut [f64]), fu8: fn(&mut [u8]), vec: PyObject) -> PyResult<PyObject> {
     Python::with_gil(|py| {
         if let Ok(mut vec) = vec.extract::<Vec<i64>>(py) {
@@ -19,7 +16,6 @@ fn _call_each_type_sort(fi: fn(&mut [i64]), ff: fn(&mut [f64]), fu8: fn(&mut [u8
             let mut vec = vec.into_bytes();
             fu8(&mut vec);
             return Ok(String::from_utf8(vec).unwrap().to_object(py));
-            //return Ok(String::from_utf8(fu8(vec.into_bytes())).unwrap().to_object(py));
         }
         Err(PyTypeError::new_err("Not supported"))
     })
@@ -30,7 +26,6 @@ pub fn bubble_sort(vec: PyObject) -> PyResult<PyObject> {
     _call_each_type_sort(_bubble_sort::<i64>, _bubble_sort::<f64>, _bubble_sort::<u8>, vec)
 }
 
-//fn _bubble_sort<T: Copy + PartialOrd>(mut vec: Vec<T>) -> Vec<T> {
 fn _bubble_sort<T: Copy + PartialOrd>(vec: &mut [T]) {
     for i in 0..vec.len()-1 {
         for j in i..vec.len() {
@@ -44,13 +39,11 @@ fn _bubble_sort<T: Copy + PartialOrd>(vec: &mut [T]) {
 }
 
 #[pyfunction]
-//pub fn selection_sort(vec: PyObject) -> PyResult<PyObject> {
-//    Python::with_gil(|py| {
-//        _call_general_sort(_bubble_sort)
-//    })
-//}
-//
-pub fn selection_sort(mut vec: Vec<i64>) -> PyResult<Vec<i64>> {
+pub fn selection_sort(vec: PyObject) -> PyResult<PyObject> {
+    _call_each_type_sort(_selection_sort::<i64>, _selection_sort::<f64>, _selection_sort::<u8>, vec)
+}
+
+pub fn _selection_sort<T: Copy + PartialOrd>(vec: &mut [T]) {
     for i in 0..vec.len()-1 {
         let mut min_index: usize = i;
         for j in i+1..vec.len() {
@@ -62,46 +55,42 @@ pub fn selection_sort(mut vec: Vec<i64>) -> PyResult<Vec<i64>> {
         vec[i] = vec[min_index];
         vec[min_index] = tmp;
     }
-
-    Ok(vec)
 }
 
 #[pyfunction]
-pub fn merge_sort(mut vec: Vec<i64>) -> PyResult<Vec<i64>> {
-    let original_len = vec.len();
-    let mut exp_len = 1;
-    loop {
-        if vec.len() < exp_len {
-            while vec.len() < exp_len {
-                vec.push(i64::MAX);
-            }
-            break;
-        }
-        else if vec.len() == exp_len {
-            break;
-        }
-        exp_len *= 2;
-    }
+pub fn merge_sort(vec: PyObject) -> PyResult<PyObject> {
+    _call_each_type_sort(_merge_sort::<i64>, _merge_sort::<f64>, _merge_sort::<u8>, vec)
+}
 
+pub fn _merge_sort<T: Copy + PartialOrd>(vec: &mut [T]) {
     let mut size = 1;
     loop {
+        let mut end_loop = false;
         size *= 2;
-        if size > vec.len() {
-            break;
+        if size >= vec.len() {
+            end_loop = true;
         }
         for i in (0..vec.len()).step_by(size) {
-            let mut small_vec = Vec::new();
+            if i + size/2 >= vec.len() {
+                continue;
+            }
+            let mut small_vec = Vec::with_capacity(size);
+            let mut right_limit = size/2;
+            if i + size >= vec.len() {
+                right_limit = vec.len() - i - size/2;
+            }
+
             let mut left_index = 0;
             let mut right_index = 0;
             loop {
                 if left_index >= size/2 {
-                    if right_index >= size/2 {
+                    if right_index >= right_limit {
                         break;
                     }
                     small_vec.push(vec[i+size/2+right_index]);
                     right_index += 1;
                 }
-                else if right_index >= size/2 {
+                else if right_index >= right_limit {
                     small_vec.push(vec[i+left_index]);
                     left_index += 1;
                 }
@@ -116,19 +105,23 @@ pub fn merge_sort(mut vec: Vec<i64>) -> PyResult<Vec<i64>> {
             }
             for j in 0..size {
                 vec[i+j] = small_vec[j];
+                if i + j >= vec.len() - 1 {
+                    break;
+                }
             }
         }
+        if end_loop {
+            break;
+        }
     }
-
-    while vec.len() > original_len {
-        vec.pop();
-    }
-
-    Ok(vec)
 }
 
 #[pyfunction]
-pub fn quick_sort(mut vec: Vec<i64>) -> PyResult<Vec<i64>> {
+pub fn quick_sort(vec: PyObject) -> PyResult<PyObject> {
+    _call_each_type_sort(_quick_sort::<i64>, _quick_sort::<f64>, _quick_sort::<u8>, vec)
+}
+
+pub fn _quick_sort<T: Copy + PartialOrd>(vec: &mut [T]) {
     struct BeginEnd {
         begin: usize,
         end: usize,
@@ -174,8 +167,6 @@ pub fn quick_sort(mut vec: Vec<i64>) -> PyResult<Vec<i64>> {
         if range.end - right >= 2 {
             queue.push_front(BeginEnd { begin: right, end: range.end });
         }
-        println!("from {} to {}: {:?}", range.begin, range.end, vec);
+        //println!("from {} to {}: {:?}", range.begin, range.end, vec);
     }
-
-    Ok(vec)
 }
