ae6f913b9c486a25fe92654a7d55c37511ebf953
Kaito Kishi <17728957+TRSasasusu@users.noreply.github.com>
2023-09-04 23:44:05 +0900

    make graph directory

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
 use pyo3::prelude::*;
 
 mod sort;
-mod dijkstra;
-mod topological_sort;
+mod graph;
 
 #[pymodule]
 fn algorithm_dict(_py: Python, m: &PyModule) -> PyResult<()> {
@@ -11,9 +10,8 @@ fn algorithm_dict(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sort::merge_sort, m)?)?;
     m.add_function(wrap_pyfunction!(sort::quick_sort, m)?)?;
 
-    m.add_function(wrap_pyfunction!(dijkstra::dijkstra, m)?)?;
-
-    m.add_function(wrap_pyfunction!(topological_sort::topological_sort, m)?)?;
+    m.add_function(wrap_pyfunction!(graph::dijkstra::dijkstra, m)?)?;
+    m.add_function(wrap_pyfunction!(graph::topological_sort::topological_sort, m)?)?;
 
     Ok(())
 }
